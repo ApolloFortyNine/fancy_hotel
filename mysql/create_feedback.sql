4483b674361fee756435dbc10a25e1da66e4005f
Ryan Quinn <fowardingtoyou@gmail.com>
2015-10-30 14:53:19 -0400

    Update create_feedback.sql

--- mysql/create_feedback.sql
+++ mysql/create_feedback.sql
@@ -3,7 +3,7 @@
   comment TEXT NULL,
   rating VARCHAR(20) NOT NULL,
   location VARCHAR(30) NOT NULL,
-  customer_id BIGINT UNSIGNED NOT NULL,
+  customer_id varchar(30) NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (customer_id) REFERENCES customers(username)
 );
