172ce002528e4c6c660e4913d9e2a241e7553a70
Ryan Quinn <fowardingtoyou@gmail.com>
2015-10-30 14:48:55 -0400

    Update create_reservations.sql

--- mysql/create_reservations.sql
+++ mysql/create_reservations.sql
@@ -5,8 +5,8 @@
   end_date DATE NOT NULL,
   extra_bed_selected BOOLEAN NOT NULL DEFAULT false,
   room_id BIGINT UNSIGNED NOT NULL,
-  user_id BIGINT UNSIGNED NOT NULL,
+  customer_id BIGINT UNSIGNED NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (room_id) REFERENCES rooms(id),
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (customer_id) REFERENCES customers(username)
 );
