f02d025d421c95bd91d30d7c025fa47ea71692fd
Ryan Quinn <fowardingtoyou@gmail.com>
2015-10-30 14:44:59 -0400

    Update create_cards.sql

--- mysql/create_cards.sql
+++ mysql/create_cards.sql
@@ -1,10 +1,9 @@
-﻿CREATE TABLE cards (
-  id SERIAL,
+CREATE TABLE cards (
+  card_number BIGINT NOT NULL,
   name_on_card VARCHAR(50) NOT NULL,
-  card_number INT NOT NULL,
   expiration_date DATE NOT NULL,
   cvv INT NOT NULL,
-  user_id BIGINT UNSIGNED NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  customer_id BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (card_number),
+  FOREIGN KEY (customer_id) REFERENCES customers(username)
 );
