6e6f1973d9bf4a45c37e31d1dd398af4778090b0
Ryan Quinn <fowardingtoyou@gmail.com>
2015-10-30 15:03:16 -0400

    Update create_reservations.sql

--- mysql/create_reservations.sql
+++ mysql/create_reservations.sql
@@ -4,9 +4,12 @@ CREATE TABLE reservations (
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
   extra_bed_selected BOOLEAN NOT NULL DEFAULT false,
+  is_cancelled BOOLEAN NOT NULL DEFAULT false,
   room_id BIGINT UNSIGNED NOT NULL,
   customer_id varchar(30) NOT NULL,
+  card_number_id BIGINT NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (room_id) REFERENCES rooms(id),
-  FOREIGN KEY (customer_id) REFERENCES customers(username)
+  FOREIGN KEY (customer_id) REFERENCES customers(username),
+  FOREIGN KEY (card_number_id) REFERENCES cards(card_number)
 );
