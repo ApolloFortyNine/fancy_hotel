be676ab994270abf94069825259823caed4a424b
Ryan Quinn <fowardingtoyou@gmail.com>
2015-10-30 14:35:56 -0400

    Update create_user.sql

--- mysql/create_user.sql
+++ mysql/create_user.sql
@@ -1,8 +1,6 @@
-﻿CREATE TABLE users (
-  id SERIAL,
-  username varchar(30) NOT NULL UNIQUE,
+CREATE TABLE customers (
+  username varchar(30),
   password varchar(30) NOT NULL,
   email varchar(50) NOT NULL,
-  manager BOOLEAN NOT NULL DEFAULT false,
-  PRIMARY KEY (id)
+  PRIMARY KEY (username)
 );
