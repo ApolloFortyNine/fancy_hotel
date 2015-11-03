efad0e5dea0a449318c9880a8ca5d5b5d1aaeecb
Ryan Quinn <fowardingtoyou@gmail.com>
2015-11-03 04:07:36 -0500

    Changed double to decimal

--- mysql/create_reservations.sql
+++ mysql/create_reservations.sql
@@ -2,7 +2,7 @@ CREATE TABLE reservations (
   id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
-  total_cost DOUBLE NOT NULL,
+  total_cost DECIMAL(10,2) NOT NULL,
   is_cancelled BOOLEAN NOT NULL DEFAULT false,
   customer_id VARCHAR(30) NOT NULL,
   card_number_id VARCHAR(20),
--- mysql/create_rooms.sql
+++ mysql/create_rooms.sql
@@ -2,8 +2,8 @@ CREATE TABLE rooms (
   room_number INTEGER NOT NULL,
   location VARCHAR(30) NOT NULL,
   room_category VARCHAR(20) NOT NULL,
-  cost_per_day DOUBLE NOT NULL,
-  cost_of_extra_bed_per_day DOUBLE NULL,
+  cost_per_day DECIMAL(10,2) NOT NULL,
+  cost_of_extra_bed_per_day DECIMAL(10,2) NULL,
   persons_allowed INTEGER NOT NULL,
   PRIMARY KEY (room_number, location)
 ) Engine = InnoDB;
