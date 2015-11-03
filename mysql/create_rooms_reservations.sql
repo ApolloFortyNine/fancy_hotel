906ad8d116d06089382029de1dde973d7f4b29f5
Ryan Quinn <fowardingtoyou@gmail.com>
2015-11-02 21:19:34 -0500

    Removed unnecessary not nulls

--- mysql/create_rooms_reservations.sql
+++ mysql/create_rooms_reservations.sql
@@ -1,7 +1,7 @@
 CREATE TABLE rooms_reservations (
   reservation_id BIGINT UNSIGNED,
-  room_number_id INTEGER NOT NULL,
-  location_id VARCHAR(30) NOT NULL,
+  room_number_id INTEGER,
+  location_id VARCHAR(30),
   extra_bed_selected BOOLEAN NOT NULL DEFAULT false,
   PRIMARY KEY (reservation_id, room_number_id, location_id),
   FOREIGN KEY (room_number_id, location_id) REFERENCES rooms(room_number, location),
