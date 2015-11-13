be923e145a88a0b478a13cbb2603a9bdf6b1d1d0
Ryan Quinn <fowardingtoyou@gmail.com>
2015-11-13 17:05:16 -0500

    Update create_customers.sql

--- mysql/create_customers.sql
+++ mysql/create_customers.sql
@@ -1,6 +1,6 @@
 CREATE TABLE customers (
   username varchar(30),
   password varchar(30) NOT NULL,
-  email varchar(50) NOT NULL,
+  email varchar(50) NOT NULL UNIQUE,
   PRIMARY KEY (username)
 ) Engine = InnoDB;
