16b1a96b631309c5e55fd3d12a1154ebdd0d5453
Ryan Quinn <fowardingtoyou@gmail.com>
2015-11-19 00:37:29 -0500

    Modified cards table because cvv's can have a leading 0

--- mysql/create_cards.sql
+++ mysql/create_cards.sql
@@ -2,7 +2,7 @@ CREATE TABLE cards (
   card_number VARCHAR(20) NOT NULL,
   name_on_card VARCHAR(50) NOT NULL,
   expiration_date DATE NOT NULL,
-  cvv INT NOT NULL,
+  cvv VARCHAR(10) NOT NULL,
   customer_id VARCHAR(30) NOT NULL,
   PRIMARY KEY (card_number),
   FOREIGN KEY (customer_id) REFERENCES customers(username)
